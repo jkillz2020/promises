4cb46d430e4b46df9b7242b4c78e0f70fcb205c3
jkillz2020 <kilgore.josh@gmail.com>
2016-10-10 19:17:29 -0500

    refactored to use promises

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,19 +4,42 @@ $(document).ready(function() {
   console.log("jquery is ready");
   var contentEl = $("#all-my-songs");
   var songs = [];
-  $.ajax({
-    url: "../songs.json"
-  }).done(function(data) {
-    songs = data.songs;
-    console.log("data", data);
-    $.ajax({
-      url: "../songs2.json"
-      }).done(function(data2) {
-        console.log("data2", data2);
-        var songData = "";
-        var currentSong;
-        
-        data2.songs.forEach(function(song){
+
+  function getSongs(){
+    return new Promise((resolve, reject) =>{
+      $.ajax({
+        url:"../songs.json"
+      }).done(function(data){
+        resolve(data);
+      }).fail(function(xhr, status, error){
+        reject(error);
+      });
+    });
+  }
+
+  function getSongs2(resultOfFirstAjax){
+    return new Promise((resolve, reject) =>{
+      $.ajax({
+        url:"../songs2.json"
+      }).done(function(data2){
+        songs = resultOfFirstAjax.songs;
+        resolve(data2);
+      }).fail(function(xhr2, status2, error2){
+        reject(error2);
+      });
+    });
+  }
+
+  getSongs().then(function(dataPass){
+    console.log("dataPass", dataPass);
+    console.log("song 1st then", songs); //empty array
+    return getSongs2(dataPass);
+  }).then(function(dataPass2){
+    console.log("song 2nd then", songs); //3 songs from 1st ajax
+var songData = "";
+var currentSong;
+
+  dataPass2.songs.forEach(function(song){
           songs.push(song);
         });
 
@@ -38,10 +61,24 @@ $(document).ready(function() {
 
         console.log("songData", songData);
         contentEl.html(songData);
-      }).fail(function(error) {
-        console.log( "error" , error);
-      });
-  }).fail(function(error) {
-    console.log( "error" , error);
-  });
+    
+    });
+  // $.ajax({
+  //   url: "../songs.json"
+  // }).done(function(data) {
+  //   songs = data.songs;
+  //   console.log("data", data);
+  //   $.ajax({
+  //     url: "../songs2.json"
+  //     }).done(function(data2) {
+  //       console.log("data2", data2);
+  //       
+        
+  //       
+  //     }).fail(function(error) {
+  //       console.log( "error" , error);
+  //     });
+  // }).fail(function(error) {
+  //   console.log( "error" , error);
+  // });
 });
